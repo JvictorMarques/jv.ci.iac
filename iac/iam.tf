68ffcd1a7bcdfe82b6dcc3cc9721e88f71b6857d
JvictorMarques <jvictorsilvamarques358@gmail.com>
2024-09-24 16:09:38 -0300

    feat: Add IAM role for Terraform
    Add an IAM role to allow Terraform to assume roles from GitHub actions. This enables Terraform to manage AWS resources using GitHub actions.
    
    The IAM role assumes roles using the GitHub OIDC provider and is restricted to the repository. This ensures secure access control for Terraform.

--- iac/iam.tf
+++ iac/iam.tf
@@ -12,6 +12,37 @@ resource "aws_iam_openid_connect_provider" "oidc-github" {
   }
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+    assume_role_policy = jsonencode(
+    {
+      Version : "2012-10-17",
+      Statement : [
+        {
+          Effect : "Allow",
+          Action : "sts:AssumeRoleWithWebIdentity",
+          Principal : {
+            "Federated" : "arn:aws:iam::864981720117:oidc-provider/token.actions.githubusercontent.com"
+          },
+          Condition : {
+            StringEquals : {
+              "token.actions.githubusercontent.com:aud" : [
+                "sts.amazonaws.com.cn"
+              ],
+              "token.actions.githubusercontent.com:sub" : [
+                "repo:JvictorMarques/pipelines.ci.api:ref:refs/heads/main"
+              ]
+            }
+          }
+        }
+      ]
+  })
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "ecr_role" {
   name = "ecr_role"
 
